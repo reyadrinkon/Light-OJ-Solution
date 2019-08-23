39f1c70299a3ed3dcc16457c9cbf9915ff28488f
Reyad Hasan Rinkon <45186655+rinkon005@users.noreply.github.com>
2019-08-23 21:42:03 +0600

    Add files via upload

---
+++ 1000 - Greetings from LightOJ .cpp
@@ -0,0 +1,25 @@
+/*
+Author:Reyad Hasan Rinkon
+SUST CSE-2017B
+*/
+    #include <iostream>
+
+    using namespace std;
+
+    int main()
+    {
+
+        int t;
+        int a;
+        int b;
+        cin >> t;
+        for (int i = 1; i <= t; i++) {
+            cin >> a;
+            cin >> b;
+
+            a = a + b;
+            cout <<"Case "<<i<<": "<< a << endl;
+        }
+
+    }
+
