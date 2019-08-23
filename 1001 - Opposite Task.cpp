0e72832ce23e16aa6482332c7a79054c0328e9ba
Reyad Hasan Rinkon <45186655+rinkon005@users.noreply.github.com>
2019-08-23 21:47:42 +0600

    Add files via upload

---
+++ 1001 - Opposite Task.cpp
@@ -0,0 +1,29 @@
+/*
+Author:Reyad Hasan Rinkon
+SUST CSE-2017B
+*/
+
+
+
+    #include <iostream>
+
+    using namespace std;
+
+    int main()
+    {
+
+        int t;
+        int a;
+        int b;
+        cin >> t;
+        for (int i = 1; i <= t; i++) {
+            cin >> a;
+
+            b = a / 2;
+            a = a % 2 + a / 2;
+            cout << a << " "<<b << endl;
+
+        }
+
+    }
+
